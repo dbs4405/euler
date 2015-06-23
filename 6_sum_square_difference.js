601d98f9703d0dc259723aacef908870e653f867
David Schiele <david.schiele@gmail.com>
2015-06-23 18:27:40 -0500

    First commit

---
+++ 6_sum_square_difference.js
@@ -0,0 +1,11 @@
+function ssd (n) {
+    var sum1 = 0, sum2 = 0;
+    for (var i = 1; i <= n; i++) {
+        sum1+=i*i;
+        sum2+= i;
+    }
+    sum2 = sum2*sum2;
+    console.log(sum2-sum1);
+}
+
+ssd(100);
